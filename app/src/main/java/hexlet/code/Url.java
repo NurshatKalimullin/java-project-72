6c2adb1fcc701908c113dcae4652d250425c5743
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2022-12-10 02:03:53 +0300

    Add model

---
+++ app/src/main/java/hexlet/code/Url.java
@@ -0,0 +1,51 @@
+package hexlet.code;
+
+import io.ebean.Model;
+import io.ebean.annotation.NotNull;
+import io.ebean.annotation.WhenCreated;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Url extends Model {
+
+    @Id
+    private int id;
+
+    @NotNull
+    private String name;
+
+    @WhenCreated
+    private Date createdAt;
+
+
+    public Url(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
