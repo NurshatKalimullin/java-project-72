669156826172a0c0807f77fdd21ec0e504aca47b
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2022-12-23 00:49:00 +0300

    Add url check controller

--- app/src/main/java/hexlet/code/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -1,14 +1,22 @@
 package hexlet.code.controllers;
 
 import hexlet.code.domain.Url;
+import hexlet.code.domain.UrlCheck;
 import hexlet.code.domain.query.QUrl;
+import hexlet.code.domain.query.QUrlCheck;
 import io.ebean.PagedList;
 import io.javalin.http.Handler;
 import io.javalin.http.HttpCode;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.Objects;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public final class UrlController {
 
@@ -38,6 +46,18 @@ public final class UrlController {
                 .id.equalTo(id)
                 .findOne();
 
+
+        PagedList<UrlCheck> pagedChecks = new QUrlCheck()
+                .setFirstRow(0)
+                .setMaxRows(1000)
+                .url.name.equalTo(url.getName())
+                .orderBy()
+                .id.asc()
+                .findPagedList();
+
+        List<UrlCheck> checks = pagedChecks.getList();
+
+        ctx.attribute("checks", checks);
         ctx.attribute("url", url);
 
         ctx.render("check.html");
@@ -45,15 +65,35 @@ public final class UrlController {
 
     public static Handler checkUrl = ctx -> {
 
-//        long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
-//
-//        Url url = new QUrl()
-//                .id.equalTo(id)
-//                .findOne();
-//
-//        ctx.attribute("url", url);
-//
-//        ctx.render("check.html");
+        long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+
+        Url url = new QUrl()
+                .id.equalTo(id)
+                .findOne();
+
+        HttpResponse<String> responseGet = Unirest
+                .post(url.getName())
+                .asString();
+
+        String content = responseGet.getBody();
+
+        Document body = Jsoup.parse(content);
+
+        String h1 = body.selectFirst("h1") != null
+                ? Objects.requireNonNull(body.selectFirst("h1")).text()
+                : null;
+        String description = body.selectFirst("meta[name=description]") != null
+                ? Objects.requireNonNull(body.selectFirst("meta[name=description]")).attr("content")
+                : null;
+
+
+        UrlCheck urlCheck = new UrlCheck(responseGet.getStatus(), body.title(), h1, description, url);
+
+        urlCheck.save();
+
+        ctx.sessionAttribute("flash", "Страница успешно проверена");
+        ctx.sessionAttribute("flash-type", "success");
+        ctx.redirect("/urls/" + id);
     };
     public static Handler createUrl = ctx -> {
 
