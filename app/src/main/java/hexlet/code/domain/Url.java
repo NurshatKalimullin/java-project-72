90b92c948b0139a32075bd74d2b9f357ab665128
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2022-12-26 13:36:11 +0300

    Fix Url

--- app/src/main/java/hexlet/code/domain/Url.java
+++ app/src/main/java/hexlet/code/domain/Url.java
@@ -8,7 +8,6 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.GeneratedValue;
 
 import java.time.Instant;
 import java.util.List;
